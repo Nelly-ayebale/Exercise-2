8f80195c08c79f80689c0f8f65f53e98986df007
Nelly-ayebale <ayebalenelly26@gmail.com>
2022-03-06 11:00:12 +0100

    Added calculation methods in the class to return data.

--- src/LegoBricks.java
+++ src/LegoBricks.java
@@ -1,2 +1,14 @@
 public class LegoBricks {
+    public static void main(String[] args) {
+        int amountOfBricks = 89;
+        int containerCapacity = 6;
+
+        int numberOfFullContainers = amountOfBricks/containerCapacity;
+        int totalNumberOfContainers = (numberOfFullContainers + 1);
+        int numberOfRemainingBlocks = amountOfBricks%containerCapacity;
+
+        System.out.println("There are"+ numberOfFullContainers+ "full containers");
+        System.out.println("There are"+ totalNumberOfContainers+ "in total");
+        System.out.println("There are"+numberOfRemainingBlocks+ "in the container that is not completely full");
+    }
 }
